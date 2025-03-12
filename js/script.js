6a1522a6fb358ddb5c31f69df406b2512bfe259a
Manel Bueno <bmanelb@gmail.com>
2025-03-12 15:28:42 +0100

    boton js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
  // Aquí tu código
+const button1 = document.getElementById("agregar");
+let resultados = document.getElementById("lista");
+
+button1.addEventListener("click", function(){
+    let añadir = prompt("Que deseas agregar?");
+    if (añadir){
+            let elementos = document.createElement("li");
+            elementos.textContent = añadir;
+            resultados.appendChild(elementos);
+    }
+
+});
+
+
+
+
+
